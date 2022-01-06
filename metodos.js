18727deb2d0992f9c75281b517528274101c3148
Pedro Lucas Souza <pedrolucasfsouza@gmail.com>
2022-01-06 20:17:10 -0300

    Update metodos.js

--- metodos.js
+++ metodos.js
@@ -1,10 +1,4 @@
 
-const forEach = (arr, func) => {
-  for (let i = 0; i < arr.length; i++){
-    func(arr[i])
-  }
-}
-
 const map = (array, func) => {
   let newArray = []
   for (let i = 0; i < array.length; i++){
@@ -28,10 +22,78 @@ const filter = (array, func) =>{
   }
   }
 
-console.log(newArray1)
+//console.log(newArray1)
 }
 
-filter([1,2,3,5,2,6], (item, index, array) => index === array.indexOf(item))
+//filter([1,2,3,5,2,6], (item, index, array) => index === array.indexOf(item))
 
-let carlos = [1,3,5,2,2]
-console.log(carlos.indexOf(2))
+//let carlos = [1,3,5,2,2]
+//console.log(carlos.indexOf(2))
+
+const convertToHex = color => {
+    const colors = {
+        blue: "#4566557",
+        black: "#4566557",
+        yellow: "#4566557",
+    }
+
+    return `ò hexadecimal para a cor ${color} é ${colors[color]}`
+}
+
+
+const colors = [
+    'blue',
+    'black',
+    'yellow'
+]
+
+const logColorMessage = color => console.log(convertToHex(color))
+
+//colors.forEach(color => logColorMessage(color))
+
+
+///estudo sobre promises
+
+const aPromise = new Promise((resolve, reject) => {
+  const aNumber = 10
+  console.log('promisse chamada')
+  setTimeout(()=>{
+    resolve(aNumber)
+  },1000)
+  
+})
+
+let divide = function (valor) {
+  return new Promise(function (resolve, reject){
+    console.log('entrou na promise')
+    setTimeout(() => {
+      let result = valor / 2
+      resolve(result)
+      console.log('dividiu',result)
+    }, 2000);
+  })
+  }
+
+  let carlos = function (valor) {
+    return new Promise(function (resolve, reject){
+      console.log('entrou na promise carlos')
+      setTimeout(() => {
+        let result = valor * 2
+        resolve(result)
+      }, 2000);
+    })
+    }
+  
+
+
+
+
+aPromise
+.then(value => value)
+.then(value => divide(value))
+.then(value => carlos(value)).then(value => console.log(value))
+
+
+
+
+
